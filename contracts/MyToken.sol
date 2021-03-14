7b1dcab11124deef178e89489a624c5fa8f60a97
jamalkheirbeik <jamalkheirbeik@gmail.com>
2021-03-14 17:26:09 +0200

    3. Delegated Transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,8 +9,18 @@ contract MyToken {
     string public symbol = "Symbol"; // symbol of the token
     string public standard = "MyToken v1.0"; // standard of the token
     uint256 public totalSupply; // total supply of coins
-    event Transfer(address indexed _from, address indexed _to, uint256 _value); // transfer event
+
+    // transfer event
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    // approve event
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf; // map where the key is (address) and the value is the balance (uint256)
+    mapping(address => mapping(address => uint256)) public allowance; // map to keep track of all the approvals made by users
 
     // constructor
     constructor(uint256 _initialSupply) public {
@@ -33,6 +43,46 @@ contract MyToken {
         // transfer event
         emit Transfer(msg.sender, _to, _value);
         // return the boolean value
+        return true;
+    }
+
+    /*
+        delegated transfer => {
+            1. approve function
+            2. transfer from function
+            3. allowance
+            4. approval event
+        }
+    */
+    // lets a specific account spend tokens from our behalf
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // handle the allowance
+        allowance[msg.sender][_spender] = _value;
+        // handle the approval event
+        emit Approval(msg.sender, _spender, _value);
+        // returns true if all the above passes
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // check for requirements (_from has enough tokens) , (allowance is big enough)
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        // change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        // update the allowance
+        allowance[_from][msg.sender] -= _value;
+        // call the transfer event
+        emit Transfer(_from, _to, _value);
+        // returns true if prev statements passes
         return true;
     }
 }
--- test/MyToken.js
+++ test/MyToken.js
@@ -1,113 +1,226 @@
-var MyToken = artifacts.require("./MyToken.sol");
+const MyToken = artifacts.require("./MyToken.sol");
 
-contract("MyToken", (accounts) => {
-  var tokenInstance;
-  // testing the token name
-  it("initializes the contract with the correct values", () => {
-    return MyToken.deployed()
-      .then((instance) => {
-        tokenInstance = instance;
-        return tokenInstance.name();
-      })
-      .then((name) => {
-        assert.equal(name, "MyToken", "has the correct name");
-        return tokenInstance.symbol();
-      })
-      .then((symbol) => {
-        assert.equal(symbol, "Symbol", "has the connect symbol");
-        return tokenInstance.standard();
-      })
-      .then((standard) => {
-        assert.equal(standard, "MyToken v1.0", "has the correct standard");
-      });
+contract("MyToken", async (accounts) => {
+  let tokenInstance;
+  // testing the token information
+  it("initializes the contract with the correct values", async () => {
+    let tokenInstance = await MyToken.deployed();
+    // testing the name
+    let name = await tokenInstance.name();
+    assert.equal(name, "MyToken", "has the correct name");
+    // testing the symbol
+    let symbol = await tokenInstance.symbol();
+    assert.equal(symbol, "Symbol", "has the correct symbol");
+    // testing the standard
+    let standard = await tokenInstance.standard();
+    assert.equal(standard, "MyToken v1.0", "has the correct standard");
   });
   // testing the total supply and the admin balance
-  it("allocates the total supply upon deployment", () => {
-    return MyToken.deployed()
-      .then((instance) => {
-        tokenInstance = instance;
-        return tokenInstance.totalSupply();
-      })
-      .then((totalSupply) => {
-        assert.equal(
-          totalSupply.toNumber(),
-          1000000,
-          "sets total supply to 1 million"
-        );
-        return tokenInstance.balanceOf(accounts[0]);
-      })
-      .then((adminBalance) => {
-        assert.equal(
-          adminBalance.toNumber(),
-          1000000,
-          "allocates the initial supply to admin account"
-        );
-      });
+  it("allocates the total supply upon deployment", async () => {
+    let tokenInstance = await MyToken.deployed();
+    // test total supply
+    let totalSupply = await tokenInstance.totalSupply();
+    assert.equal(
+      totalSupply.toNumber(),
+      1000000,
+      "sets total supply to 1 million"
+    );
+    // test admin balance
+    let adminBalance = await tokenInstance.balanceOf(accounts[0]);
+    assert.equal(
+      adminBalance.toNumber(),
+      1000000,
+      "sets admin balance to 1 million"
+    );
   });
   // testing the transfer ownership
-  it("transfer token ownership", () => {
-    return MyToken.deployed()
-      .then((instance) => {
-        tokenInstance = instance;
-        // test if the tokens transfered are larger than the sender's balance
-        return tokenInstance.transfer.call(accounts[1], 99999999999999999);
-      })
-      .then(assert.fail)
-      .catch((error) => {
-        assert(error.message, "error message must contain revert");
-        // fake transaction to determinate if the real one would be successful or not
-        return tokenInstance.transfer
-          .call(accounts[1], 250000, {
-            from: accounts[0],
-          })
-          .then((success) => {
-            // check if the transfer was successful (success) value is passed from the prev fake transaction
-            assert.equal(success, true, "it returns true");
-            return tokenInstance.transfer(accounts[1], 250000, {
-              from: accounts[0],
-            });
-          })
-          .then((receipt) => {
-            // check for the event
-            assert.equal(receipt.logs.length, 1, "triggers one event");
-            // check if the event have all the arguements we expect (from, to, value)
-            assert.equal(
-              receipt.logs[0].event,
-              "Transfer",
-              'should be the "Transfer" event'
-            );
-            assert.equal(
-              receipt.logs[0].args._from,
-              accounts[0],
-              "logs the account the tokens are transferred from"
-            );
-            assert.equal(
-              receipt.logs[0].args._to,
-              accounts[1],
-              "logs the account the tokens are transferred to"
-            );
-            assert.equal(
-              receipt.logs[0].args._value,
-              250000,
-              "logs the transfer amount"
-            );
-            return tokenInstance.balanceOf(accounts[1]);
-          })
-          .then((balance) => {
-            assert.equal(
-              balance.toNumber(),
-              250000,
-              "adds the amount to the receiving account"
-            );
-            return tokenInstance.balanceOf(accounts[0]);
-          })
-          .then((balance) => {
-            assert.equal(
-              balance.toNumber(),
-              750000,
-              "deducts the amount from the sending account"
-            );
-          });
+  it("transfer token ownership", async () => {
+    let tokenInstance = await MyToken.deployed();
+    // test if the tokens transfered are larger than the sender's balance
+    try {
+      await tokenInstance.transfer.call(accounts[1], 9999999999);
+      assert.fail("fake transaction should've thrown an error");
+    } catch (error) {
+      assert(
+        error.message.includes("revert"),
+        "error message must contain revert"
+      );
+    }
+    // test if the fake transfer will be successful
+    let success = await tokenInstance.transfer.call(accounts[1], 250000, {
+      from: accounts[0],
+    });
+    assert.equal(success, true, "it returns true");
+    // test the receipt of the actual transfer
+    let receipt = await tokenInstance.transfer(accounts[1], 250000, {
+      from: accounts[0],
+    });
+    // check for the event
+    assert.equal(receipt.logs.length, 1, "triggers one event");
+    // check if the event have all the arguements we expect (from, to, value)
+    assert.equal(
+      receipt.logs[0].event,
+      "Transfer",
+      'should be the "Transfer" event'
+    );
+    assert.equal(
+      receipt.logs[0].args._from,
+      accounts[0],
+      "logs the account the tokens are transferred from"
+    );
+    assert.equal(
+      receipt.logs[0].args._to,
+      accounts[1],
+      "logs the account the tokens are transferred to"
+    );
+    assert.equal(
+      receipt.logs[0].args._value,
+      250000,
+      "logs the transfer amount"
+    );
+    // test the added balance
+    let addedBalance = await tokenInstance.balanceOf(accounts[1]);
+    assert.equal(
+      addedBalance.toNumber(),
+      250000,
+      "adds the amount to the receiving account"
+    );
+    // test the deducted balance
+    let deductedBalance = await tokenInstance.balanceOf(accounts[0]);
+    assert.equal(
+      deductedBalance.toNumber(),
+      750000,
+      "deducts the amount from the sending account"
+    );
+  });
+  // testing the delegated transfer approval
+  it("approves tokens for delegated transfer", async () => {
+    let tokenInstance = await MyToken.deployed();
+    // test the success of the transfer
+    let success = await tokenInstance.approve.call(accounts[1], 100);
+    assert.equal(success, true, "it returns true");
+    let receipt = await tokenInstance.approve(accounts[1], 100, {
+      from: accounts[0],
+    });
+    // check for the event
+    assert.equal(receipt.logs.length, 1, "triggers one event");
+    // check if the event have all the arguements we expect (from, to, value)
+    assert.equal(
+      receipt.logs[0].event,
+      "Approval",
+      'should be the "Approval" event'
+    );
+    assert.equal(
+      receipt.logs[0].args._owner,
+      accounts[0],
+      "logs the account the tokens are authorized by"
+    );
+    assert.equal(
+      receipt.logs[0].args._spender,
+      accounts[1],
+      "logs the account the tokens are authorized to"
+    );
+    assert.equal(receipt.logs[0].args._value, 100, "logs the transfer amount");
+    // check the allowance
+    let allowance = await tokenInstance.allowance(accounts[0], accounts[1]);
+    assert.equal(
+      allowance.toNumber(),
+      100,
+      "stores the allowance for delegated transfer"
+    );
+  });
+  // handling the delegated transfer
+  it("handles the delegated transfer", async () => {
+    let tokenInstance = await MyToken.deployed();
+    // accounts
+    let fromAccount = accounts[2];
+    let toAccount = accounts[3];
+    let spendingAccount = accounts[4];
+    // transfer some tokens to the from account
+    await tokenInstance.transfer(fromAccount, 100, {
+      from: accounts[0],
+    });
+    // approve spending accounts to spend 10 tokens from (fromAccount)
+    await tokenInstance.approve(spendingAccount, 10, {
+      from: fromAccount,
+    });
+    // try transfering something larger than senders balance
+    try {
+      await tokenInstance.transferFrom(fromAccount, toAccount, 9999, {
+        from: spendingAccount,
       });
+      assert.fail("fake transaction should've thrown an error");
+    } catch (error) {
+      assert(
+        error.message.includes("revert"),
+        "cannot transfer value larger than the balance"
+      );
+    }
+    // try transfering something larger than the approved amount
+    try {
+      await tokenInstance.transferFrom(fromAccount, toAccount, 20, {
+        from: spendingAccount,
+      });
+      assert.fail("should throw an error");
+    } catch (error) {
+      assert(
+        error.message.includes("revert"),
+        "cannot transfer value larger than the approved amount"
+      );
+    }
+    // test the returned value (succeed or not)
+    let success = await tokenInstance.transferFrom.call(
+      fromAccount,
+      toAccount,
+      10,
+      {
+        from: spendingAccount,
+      }
+    );
+    assert.equal(success, true);
+    // test the transfer event
+    let receipt = await tokenInstance.transferFrom(fromAccount, toAccount, 10, {
+      from: spendingAccount,
+    });
+    // check for the event
+    assert.equal(receipt.logs.length, 1, "triggers one event");
+    // check if the event have all the arguements we expect (from, to, value)
+    assert.equal(
+      receipt.logs[0].event,
+      "Transfer",
+      'should be the "Transfer" event'
+    );
+    assert.equal(
+      receipt.logs[0].args._from,
+      fromAccount,
+      "logs the account the tokens are transfered from"
+    );
+    assert.equal(
+      receipt.logs[0].args._to,
+      toAccount,
+      "logs the account the tokens are transfered to"
+    );
+    assert.equal(receipt.logs[0].args._value, 10, "logs the transfer amount");
+    // test changing the balance of sending account
+    let fromBalance = await tokenInstance.balanceOf(fromAccount);
+    assert.equal(
+      fromBalance.toNumber(),
+      90,
+      "deducts the amount from the sending account"
+    );
+    // test changing the balance of receiving account
+    let toBalance = await tokenInstance.balanceOf(toAccount);
+    assert.equal(
+      toBalance.toNumber(),
+      10,
+      "adds the amount to the receiving account"
+    );
+    // test the allowance
+    let allowance = await tokenInstance.allowance(fromAccount, spendingAccount);
+    assert.equal(
+      allowance.toNumber(),
+      0,
+      "deducts the amount from the allowance"
+    );
   });
 });
